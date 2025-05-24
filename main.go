238c65d2c4ddda2c9014b3c37ce05680ed9f0343
quockhanhcao <quockhanhcao99@gmail.com>
2025-05-24 17:56:27 +0700

    feat: Add some more API routes

--- main.go
+++ main.go
@@ -1,26 +1,42 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileServerHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileServerHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
 
 func main() {
 	const filePathRoot = "."
 	const port = "8080"
+	var apiCfg = apiConfig{}
 
 	mux := http.NewServeMux()
 
-    // file server
-    fileServerHandler := http.StripPrefix("/app", http.FileServer(http.Dir(filePathRoot)))
-	mux.Handle("/app/", fileServerHandler)
+	// file server
+	fileServerHandler := http.StripPrefix("/app", http.FileServer(http.Dir(filePathRoot)))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(fileServerHandler))
 
-    // route handler
-    mux.HandleFunc("/healthz", readinessHandler)
+	// route handler
+	mux.HandleFunc("GET /api/healthz", readinessHandler)
+	mux.HandleFunc("POST /admin/reset", apiCfg.resetMetricsHandler)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.requestCountHandler)
 
-    // start the server
+	// start the server
 	server := &http.Server{
-        Addr:    ":" + port,
+		Addr:    ":" + port,
 		Handler: mux,
 	}
 	log.Printf("Serving files from %s on port: %s\n", filePathRoot, port)
@@ -28,7 +44,18 @@ func main() {
 }
 
 func readinessHandler(w http.ResponseWriter, r *http.Request) {
-    w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-    w.WriteHeader(http.StatusOK)
-    w.Write([]byte("OK\n"))
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK\n"))
+}
+
+func (cfg *apiConfig) requestCountHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	count := cfg.fileServerHits.Load()
+	w.Write([]byte(fmt.Sprintf("<html>\n<body>\n<h1>Welcome, Chirpy Admin</h1>\n<p>Chirpy has been visited %d times!</p>\n</body></html>", count)))
+}
+
+func (cfg *apiConfig) resetMetricsHandler(w http.ResponseWriter, r *http.Request) {
+	cfg.fileServerHits.Store(0)
 }
